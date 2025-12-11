6e9eeead64eb3e8a58e5f8fc792bfce68f01251e
= <=>
2025-12-11 08:26:01 -0500

    fix sitemap filtering

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import {
-  scrape_location_urls,
+  scrape_location_pages,
   scrape_corporate_urls,
   load_sitemap,
 } from "./components/scraper.js";
@@ -13,16 +13,24 @@ import corporate_urls from "./corporate.json" with {type: "json" };
   const sitemap = await load_sitemap(
     `https://www.servicemasterrestore.com/sitemap.xml`
   );
+  
+  uat_group.forEach(async (location) => {
+    // grab this locations sub pages
+    location.pages = sitemap.filter(page => page.path.includes(location.scorpion_url));
+    console.log(location.pages);
+    exit();
+
+    const loc_results = await scrape_location_pages(location);
+    // console.log(results);
+  });
+  
+
+
 
   /*
-  uat_group.forEach(async (location) => {
-    location.pages = sitemap.filter((url) => url.includes(location.scorpion_url));
-    const loc_results = await scrape_location_urls(location);
-    // console.log(results);
-  });*/
-
   const filtered = sitemap.filter((obj) => 
     corporate_urls.some((val) => obj.path.includes(val))
   ); 
   const corp_results =  await scrape_corporate_urls(filtered);
+  */
 })();
