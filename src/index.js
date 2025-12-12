f77a55f9b879ef3a8ebc136eb21c4dde4d42f066
= <=>
2025-12-11 22:46:32 -0500

    return all content and write in a single go

--- src/index.js
+++ src/index.js
@@ -5,28 +5,29 @@ import {
 } from "./components/scraper.js";
 
 import locations from "./locations.json"  with { type: "json" };
-const uat_group = locations.filter((l) => l.uat_group === true);
+const uat_group = locations.filter((l) => l.test_group === true);
+
+import * as utilities from "./components/utilities.js";
 
 import corporate_urls from "./corporate.json" with {type: "json" };
+import { utimes } from "fs";
 
 (async () => {
   const sitemap = await load_sitemap(
     `https://www.servicemasterrestore.com/sitemap.xml`
   );
   
-  uat_group.forEach(async (location) => {
-    // grab this locations sub pages
-    location.pages = sitemap.filter(page => page.path.includes(location.scorpion_url));
-    console.log(location.pages);
-    exit();
+  // find matching pages from the sitemap
+  const results = uat_group.map( l => ({
+    ...l,
+    pages: sitemap.filter(p => p.path.includes(l.scorpion_url))
+  }));  
 
-    const loc_results = await scrape_location_pages(location);
-    // console.log(results);
-  });
-  
+  const content_rows = (await scrape_location_pages(results)).flat();
 
-
-
+  await utilities.write_csv("../outputs.pages.txt");
+  await utilities.write_csv("../outputs/pages.txt", Object.keys(content_rows[0]));
+  await utilities.write_csv("../outputs/pages.txt",content_rows.flat());
   /*
   const filtered = sitemap.filter((obj) => 
     corporate_urls.some((val) => obj.path.includes(val))
