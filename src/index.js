d176aaa1e6c5642fba613fd09129ae13c4b0a6a2
Caleb Williams <caleb616@gmail.com>
2026-01-02 15:54:40 -0500

    cleanup strange imports from VS

--- src/index.js
+++ src/index.js
@@ -10,24 +10,24 @@ const uat_group = locations.filter((l) => l.test_group === true);
 import * as utilities from "./components/utilities.js";
 
 import corporate_urls from "./corporate.json" with {type: "json" };
-import { utimes } from "fs";
 
 (async () => {
   const sitemap = await load_sitemap(
     `https://www.servicemasterrestore.com/sitemap.xml`
   );
-  
+
   // find matching pages from the sitemap
-  const results = uat_group.map( l => ({
+  const results = uat_group.map(l => ({
     ...l,
     pages: sitemap.filter(p => p.path.includes(l.scorpion_url))
-  }));  
+  }));
 
   const content_rows = (await scrape_location_pages(results)).flat();
 
   await utilities.write_csv("../outputs/pages.txt");                                    // clear text file
   await utilities.write_csv("../outputs/pages.txt", Object.keys(content_rows[0]));      // write headers to text file
   await utilities.write_csv("../outputs/pages.txt", content_rows);                      // write content to text file
+
   /*
   const filtered = sitemap.filter((obj) => 
     corporate_urls.some((val) => obj.path.includes(val))
