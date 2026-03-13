c730b17d01253fbad326d86429a3bfb6c64bc533
= <=>
2026-03-12 20:35:52 -0400

    more elements to skip

--- src/components/scrapers/basic.js
+++ src/components/scrapers/basic.js
@@ -39,12 +39,14 @@ export default async (page) => {
       "#AccessibilityZone",
       "#LocalMapDisplay",
       "#CalloutV2B",
+      "CalloutV5",
       "#KnowUsPopOut",
       "#help_center_widget_con",
       "LocalArticlesV1",
       ".text-highlight.v2",
       ".thin.anm_fl.ui-repeater",
-      "Form_LocalContactFormV1"
+      "Form_LocalContactFormV1",
+      ".text-highlight",
     ];
 
     selectors.forEach((sel) => $(sel).remove());
